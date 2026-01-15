93d9972a81efc10db262e658a81bbca666bab924
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2026-01-15 10:06:12 +0100

    create database and tables

--- config/Database.php
+++ config/Database.php
@@ -1,42 +1,39 @@
 <?php
     require_once __DIR__ . '/../vendor/autoload.php';
     use Dotenv\Dotenv;
-    // use PDO;
-    // use PDOException;
     
-    
-class Database {
-    private string $username;
-    private string $password;
-    private string $dbname;
-    private string $host;
+    class Database {
+        private string $username;
+        private string $password;
+        private string $dbname;
+        private string $host;
 
-    public function __construct(){
-        $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
-        $dotenv->load();
+        public function __construct(){
+            $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+            $dotenv->load();
 
-        $this->username = $_ENV['USER_NAME'];
-        $this->password = $_ENV['PASS'];
-        $this->dbname = $_ENV['DB_NAME'];
-        $this->host = $_ENV['HOST'];
-    }
+            $this->username = $_ENV['USER_NAME'];
+            $this->password = $_ENV['PASS'];
+            $this->dbname = $_ENV['DB_NAME'];
+            $this->host = $_ENV['HOST'];
+        }
 
-    public function connect(): PDO {
-        try {
-            return new PDO(
-                "pgsql:host={$this->host};dbname={$this->dbname}",
-                $this->username,
-                $this->password,
-                [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                ]
-            );
-        } catch (PDOException $e) {
-            die("DB error: " . $e->getMessage());
+        public function connect(): PDO {
+            try {
+                return new PDO(
+                    "pgsql:host={$this->host};dbname={$this->dbname}",
+                    $this->username,
+                    $this->password,
+                    [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                    ]
+                );
+            } catch (PDOException $e) {
+                die("DB error: " . $e->getMessage());
+            }
         }
     }
-}
 
 // $db = new Database();
 // $pdo = $db->connect();
